53873a8870632b19a2feb1a3dcc37e80d8096eef
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2022-03-02 17:10:32 -0600

    created ItemAdapter.kt and list_item.xml for displaying the recycler view that we will have full of affirmations

--- app/src/main/java/com/heady/games/MainActivity.kt
+++ app/src/main/java/com/heady/games/MainActivity.kt
@@ -2,10 +2,15 @@ package com.heady.games
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.TextValueSanitizer
+import android.widget.TextView
+import com.heady.games.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
